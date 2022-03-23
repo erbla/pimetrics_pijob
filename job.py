7af10f679b3a21aa38ab99cd6c57366a70f86dfd
Eric Blais <er.blais@gmail.com>
2022-03-23 18:49:08 -0400

    testing on same computer right now

--- job.py
+++ job.py
@@ -1,11 +1,11 @@
-from  datetime import datetime
-from time import sleep
+import datetime
+import time
 import requests
 
 while True:
-    body = {'lastping': datetime.now()}
+    body = {'lastping': datetime.datetime.now()}
     print(f'Sent: {body}')
-    r = requests.post('http://127.0.0.1:5000/time', body)
+    r = requests.post('http://localhost:5000/time', body)
     #print(f'Received: {r.json()}')
     print(f'Received: {r.text}')
-    sleep(3)
+    time.sleep(3)
