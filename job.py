253062333f349b8d81d1f118261450c404ab2ecb
Eric Blais <er.blais@gmail.com>
2022-03-24 16:53:23 -0400

    added a try-catch

--- job.py
+++ job.py
@@ -5,7 +5,9 @@ import requests
 while True:
     body = {'lastping': datetime.datetime.now()}
     print(f'Sent: {body}')
-    r = requests.post('http://localhost:5000/time', body)
-    #print(f'Received: {r.json()}')
+    try:
+        r = requests.post('http://localhost:5000/time', body)
+    except:
+        print(f'Error sending request to server')
     print(f'Received: {r.text}')
     time.sleep(3)
