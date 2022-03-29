ba4e27e074c8cb2420828f76cc59ecbf0d864fff
Eric Blais <er.blais@gmail.com>
2022-03-29 18:01:20 -0400

    now sending more metrics

--- job.py
+++ job.py
@@ -1,13 +1,19 @@
 import datetime
 import time
 import requests
+import psutil
 
 while True:
-    body = {'lastping': datetime.datetime.now()}
+    body = {
+        'lastping': datetime.datetime.now(),
+        'cpu-usage': psutil.cpu_percent(),
+        'ram-usage': psutil.virtual_memory().percent,
+        'disk-usage': psutil.disk_usage('/').percent,
+        }
     print(f'Sent: {body}')
     try:
-        r = requests.post('http://localhost:5000/time', body)
+        r = requests.post('http://localhost:5000/update', body)
+        print(f'Received: {r.text}')
     except:
         print(f'Error sending request to server')
-    print(f'Received: {r.text}')
-    time.sleep(3)
+    time.sleep(5)
